520977502e5706e9084042fa2d46f9bd528046ce
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2023-05-18 12:36:40 -0700

    add sass

--- gong-countries/pages/index.vue
+++ gong-countries/pages/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="container">
+  <div class="filter_results">
     <ul v-if="filtered.length">
-      <li v-for="user in filtered">{{user.first_name}} {{user.last_name}}, {{user.country}}</li>
+      <li v-for="user in filtered">
+        {{ user.first_name }} {{ user.last_name }}, {{ user.country }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({$axios}) {
-    const filtered = await $axios.$get('/api/records.php');
-    return { filtered }
-  }
-}
+  async asyncData({ $axios }) {
+    const filtered = await $axios.$get("/api/records.php");
+    return { filtered };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -21,42 +23,9 @@ export default {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.filter_results {
+  li {
+    list-style: none;
+  }
 }
 </style>
