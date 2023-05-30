41f7bf685e598cb48023b8cff0536e4e964b94c1
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2023-05-30 12:36:17 -0700

    add tailwind

--- gong-countries/nuxt.config.js
+++ gong-countries/nuxt.config.js
@@ -1,30 +1,29 @@
 export default {
+  devServerHandlers: [],
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'gong-countries',
+    title: "gong-countries",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -32,24 +31,32 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    // '@nuxtjs/tailwindcss',
+    "tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    proxy: true
+    proxy: true,
   },
   proxy: {
-    '/api/': { target: 'http://localhost:8888', pathRewrite: {'^/api/': ''} }
+    "/api/": { target: "http://localhost:8888", pathRewrite: { "^/api/": "" } },
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
   },
   devServerHandlers: [],
-}
+};
--- gong-countries/pages/index.vue
+++ gong-countries/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <ul class="filter_results" v-if="filtered.length">
-      <li v-for="user in filtered">
+    <ul class="filter_results flex flex-wrap w-full" v-if="filtered.length">
+      <li v-for="user in filtered" class="border-rounded-xl w-full md:w-1/2 lg:w-1/3">
         {{ user.first_name }} {{ user.last_name }}, {{ user.country }}
       </li>
     </ul>
@@ -23,30 +23,30 @@ export default {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.filter_results {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100vw;
-  padding: 0;
-  justify-content: center;
-  li {
-    list-style: none;
-    width: 100%;
-    height: 200px;
-    border-radius: 20px;
-    background-color: #f5edff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 5px;
-    @media (min-width: 600px) {
-      width: calc(50% - 20px);
-    }
-    @media (min-width: 1200px) {
-      width: calc(33% - 20px);
-    }
-  }
-}
+// .filter_results {
+//   display: flex;
+//   flex-wrap: wrap;
+//   width: 100vw;
+//   padding: 0;
+//   justify-content: center;
+//   li {
+//     list-style: none;
+//     width: 100%;
+//     height: 200px;
+//     border-radius: 20px;
+//     background-color: #f5edff;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     margin: 5px;
+//     @media (min-width: 600px) {
+//       width: calc(50% - 20px);
+//     }
+//     @media (min-width: 1200px) {
+//       width: calc(33% - 20px);
+//     }
+//   }
+// }
 </style>
 
   }
